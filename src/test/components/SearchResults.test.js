c070507a55db801dab80f82fda5a1a39d77a5d86
Ian Wang <gogoenwon@gmail.com>
2018-01-18 18:33:33 +0800

    add basic unit test

---
+++ src/test/components/SearchResults.test.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import Immutable from 'immutable'
+import { shallow } from 'enzyme'
+import SearchResults from 'components/SearchResults'
+
+describe('Components::SearchResults', () => {
+  let props
+  beforeEach(() => {
+    props = {
+      repos: Immutable.fromJS({
+        lang: 'lang',
+        totalCount: 2,
+        items: [{
+          id: 1
+        }, {
+          id: 2
+        }]
+      })
+    }
+  })
+
+  function renderDoc() {
+    return shallow(<SearchResults {...props} />)
+  }
+
+  it('renders all items', () => {
+    let doc = renderDoc()
+    let node = doc.find('li')
+    expect(node).toHaveLength(props.repos.get('items').size)
+  })
+})
