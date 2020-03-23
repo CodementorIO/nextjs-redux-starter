0f4ff1d002df100592b7b047afd15510ddd071e9
ianwang <ian.w@canva.com>
2020-03-23 18:03:12 +1100

    remove webpack-bundle-analyzer

--- next.config.js
+++ next.config.js
@@ -1,4 +1,4 @@
-const { ANALYZE, ASSET_HOST } = process.env
+const { ASSET_HOST } = process.env
 
 // for those who using CDN
 const assetPrefix = ASSET_HOST || ''
@@ -8,15 +8,6 @@ module.exports = {
   target: 'serverless',
   webpack: (config, { dev }) => {
     config.output.publicPath = `${assetPrefix}${config.output.publicPath}`
-
-    if (ANALYZE) {
-      const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer')
-      config.plugins.push(new BundleAnalyzerPlugin({
-        analyzerMode: 'server',
-        analyzerPort: 8888,
-        openAnalyzer: true
-      }))
-    }
 
     return config
   }
